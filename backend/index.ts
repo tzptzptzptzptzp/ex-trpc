4ceb08e0fee39835fbb5cde94169c629dd37e533
tzptzptzptzptzp <git@tzp.pink>
2023-12-04 22:57:06 +0900

    🤡 tRPCルーティングテストを検証

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.get("/", (req, res) => res.send("Hello World"));
 const t = initTRPC.create();
 const router = t.router;
 const publicProcedure = t.procedure;
+
+const appRouter = router({
+  test: publicProcedure.query(() => {
+    return "TEST tRPC";
+  }),
+});
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server Running http://localhost:${PORT}`);
