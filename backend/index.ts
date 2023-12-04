dd15d58c020b18f3d6db52c06928d7a4976f610c
tzptzptzptzptzp <git@tzp.pink>
2023-12-04 22:53:29 +0900

    ✨ tRPCを初期化

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { initTRPC } from "@trpc/server";
 
 const app = express();
 
 const PORT = 2222;
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+const t = initTRPC.create();
+const router = t.router;
+const publicProcedure = t.procedure;
 
 app.listen(PORT, () => {
   console.log(`Server Running http://localhost:${PORT}`);
