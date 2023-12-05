3561ceabb8a6a8ef2248a2d67eeaabfa2dfa8556
tzptzptzptzptzp <git@tzp.pink>
2023-12-05 20:46:25 +0900

    🤡 TodoListモックデータを作成

---
+++ backend/db/TodoList/index.ts
@@ -0,0 +1,16 @@
+import { TodoType } from "./type";
+
+export const TodoList: TodoType[] = [
+  {
+    id: "1",
+    content: "Todo 1",
+  },
+  {
+    id: "2",
+    content: "Todo 2",
+  },
+  {
+    id: "3",
+    content: "Todo 3",
+  },
+];
